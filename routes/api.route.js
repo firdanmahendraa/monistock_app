3587dee05677be5425030c8278cd5d00eba8197a
Firdan Mahendra Wardana <firdanmahendraw@gmail.com>
2023-05-25 01:12:14 +0700

    crud delivery order

--- routes/api.route.js
+++ routes/api.route.js
@@ -6,6 +6,7 @@ router.get('/', async (req, res, next) => {
   res.send({ message: 'Ok api is working 🚀' });
 });
 
+// ----------- Supplier -----------
 // Get semua isi tabel supplier
 router.get('/supplier', async (req, res) => {
   const supplier = await prisma.Mst_supplier.findMany()
@@ -56,7 +57,6 @@ router.patch('/supplier/:id', async (req, res, next) => {
   }
   });
   
-
 // Delete supplier
 router.delete('/supplier/:id', async (req, res, next) => {
   try {
@@ -72,7 +72,8 @@ router.delete('/supplier/:id', async (req, res, next) => {
   }
   }); 
 
-  // Get semua rack
+// ----------- Rack -----------
+// Get semua rack
 router.get('/rack', async (req, res) => {
   const rack = await prisma.Mst_rack.findMany()
   res.json(rack)
@@ -93,7 +94,7 @@ router.get('/rack/:id', async (req, res, next) => {
   }
   });
 
-  // Create rack
+// Create rack
 router.post('/rack', async (req, res, next) => {
   try {
     const rack = await prisma.Mst_rack.create({
@@ -138,174 +139,268 @@ router.delete('/rack/:id', async (req, res, next) => {
   }
   });
 
-  //get all part
-  router.get('/part', async (req, res, next) => {
-    try {
-        const mst_parts = await prisma.Mst_part.findMany({
-        include: {mst_racks: true}
-        })
-    
-        const mst_racks = await prisma.Mst_rack.findMany({
-        include:{mst_parts: true}
-        
-        })
-
-        res.json({mst_parts, mst_racks})
-    } catch (error) {
-        next(error)
-    }
-    });
-
-    //get part by id
-    router.get('/part/:id', async (req, res, next) => {
-      try {
-        const {id} = req.params
-        const part = await prisma.Mst_part.findUnique({
-          where:{
-            part_no: Number(id)
-          },
-          include: {mst_racks:true}
-        })
-        res.json(part)
-      } catch (error) {
-        next(error)
-      }
-    });
-
-    //create part
-    router.post('/part', async (req, res, next) => {
-      try {
-          const part = await prisma.Mst_part.create({
-          data : req.body
-          })
+// ----------- Part -----------
+//get all part
+router.get('/part', async (req, res, next) => {
+  try {
+      const mst_parts = await prisma.Mst_part.findMany({
+      include: {mst_racks: true}
+      })
+  
+      const mst_racks = await prisma.Mst_rack.findMany({
+      include:{mst_parts: true}
       
-          res.json({part})
-      } catch (error) {
-          next(error)
-      }
-      });
-
-  //update part by id
-  router.patch('/part/:id', async (req, res, next) => {
-    try {
-      const {id} = req.params
-      const part = await prisma.Mst_part.update({
-        data: req.body,
-        where:{
-          part_no: Number(id)
-        },
-        include: {mst_racks:true}
       })
-      res.json(part)
-    } catch (error) {
-      next(error)
-    }
-  }); 
 
-  //delete part by id
-  router.delete('/part/:id', async (req, res, next) => {
-    try {
-      const {id} = req.params
-      const part = await prisma.Mst_part.delete({
-        where:{
-          part_no: Number(id)
-        }
-      })
-      res.json(part)
-    } catch (error) {
+      res.json({mst_parts, mst_racks})
+  } catch (error) {
       next(error)
-    }
+  }
   });
 
-  //get all tb_wo
-  router.get('/wo', async (req, res, next) => {
-    try {
-      const tb_wos = await prisma.tb_wo.findMany({
-        include: {
-          mst_parts: true,
-          mst_suppliers: true,
-        },
-      });
-    
-        const mst_parts = await prisma.Mst_part.findMany({
-        include:{tb_wo: true}
-        })
+//get part by id
+router.get('/part/:id', async (req, res, next) => {
+  try {
+    const {id} = req.params
+    const part = await prisma.Mst_part.findUnique({
+      where:{
+        part_no: Number(id)
+      },
+      include: {mst_racks:true}
+    })
+    res.json(part)
+  } catch (error) {
+    next(error)
+  }
+});
 
-        const mst_suppliers = await prisma.Mst_supplier.findMany({
-        include:{tb_wos: true}
-        })
-
-        res.json({tb_wos, mst_parts, mst_suppliers})
-    } catch (error) {
-        next(error)
-    }
-    });
-
-    //get tb_wo by id
-    router.get('/wo/:id', async (req, res, next) => {
-      try {
-        const {id} = req.params
-        const wo = await prisma.tb_wo.findUnique({
-          where:{
-            id_wo: Number(id)
-          },
-          include: {
-            mst_parts: true,
-            mst_suppliers: true,
-          }
-        })
-        res.json(wo)
-      } catch (error) {
-        next(error)
-      }
-    });
-
-    //create wo
-    router.post('/wo', async (req, res, next) => {
-      try {
-          const wo = await prisma.Tb_wo.create({
-          data : req.body
-          })
-      
-          res.json({wo})
-      } catch (error) {
-          next(error)
-      }
-      });
-
-    //update wo by id
-    router.patch('/wo/:id', async (req, res, next) => {
-      try {
-        const {id} = req.params
-        const wo = await prisma.Tb_wo.update({
-          data: req.body,
-          where:{
-            id_wo: Number(id)
-          },
-          include: {
-            mst_parts: true,
-            mst_suppliers: true,
-          }
-        })
-        res.json(wo)
-      } catch (error) {
-        next(error)
-      }
-    }); 
-
-  //delete wo by id
-  router.delete('/wo/:id', async (req, res, next) => {
-    try {
-      const {id} = req.params
-      const wo = await prisma.Tb_wo.delete({
-        where:{
-          id_wo: Number(id)
-        }
+//create part
+router.post('/part', async (req, res, next) => {
+  try {
+      const part = await prisma.Mst_part.create({
+      data : req.body
       })
-      res.json(wo)
-    } catch (error) {
+  
+      res.json({part})
+  } catch (error) {
       next(error)
-    }
+  }
   });
+
+//update part by id
+router.patch('/part/:id', async (req, res, next) => {
+  try {
+    const {id} = req.params
+    const part = await prisma.Mst_part.update({
+      data: req.body,
+      where:{
+        part_no: Number(id)
+      },
+      include: {mst_racks:true}
+    })
+    res.json(part)
+  } catch (error) {
+    next(error)
+  }
+}); 
+
+//delete part by id
+router.delete('/part/:id', async (req, res, next) => {
+  try {
+    const {id} = req.params
+    const part = await prisma.Mst_part.delete({
+      where:{
+        part_no: Number(id)
+      }
+    })
+    res.json(part)
+  } catch (error) {
+    next(error)
+  }
+});
+
+// ----------- Work Order -----------
+//get all tb_wo
+router.get('/wo', async (req, res, next) => {
+  try {
+    const tb_wos = await prisma.tb_wo.findMany({
+      include: {
+        mst_parts: true,
+        mst_suppliers: true,
+      },
+    });
+  
+      const mst_parts = await prisma.Mst_part.findMany({
+      include:{tb_wo: true}
+      })
+
+      const mst_suppliers = await prisma.Mst_supplier.findMany({
+      include:{tb_wos: true}
+      })
+
+      res.json({tb_wos, mst_parts, mst_suppliers})
+  } catch (error) {
+      next(error)
+  }
+  });
+
+//get tb_wo by id
+router.get('/wo/:id', async (req, res, next) => {
+  try {
+    const {id} = req.params
+    const wo = await prisma.Tb_wo.findUnique({
+      where:{
+        id_wo: Number(id)
+      },
+      include: {
+        mst_parts: true,
+        mst_suppliers: true,
+      }
+    })
+    res.json(wo)
+  } catch (error) {
+    next(error)
+  }
+});
+
+//create wo
+router.post('/wo', async (req, res, next) => {
+  try {
+      const wo = await prisma.Tb_wo.create({
+      data : req.body
+      })
+  
+      res.json({wo})
+  } catch (error) {
+      next(error)
+  }
+  });
+
+//update wo by id
+router.patch('/wo/:id', async (req, res, next) => {
+  try {
+    const {id} = req.params
+    const wo = await prisma.Tb_wo.update({
+      data: req.body,
+      where:{
+        id_wo: Number(id)
+      },
+      include: {
+        mst_parts: true,
+        mst_suppliers: true,
+      }
+    })
+    res.json(wo)
+  } catch (error) {
+    next(error)
+  }
+}); 
+
+//delete wo by id
+router.delete('/wo/:id', async (req, res, next) => {
+  try {
+    const {id} = req.params
+    const wo = await prisma.Tb_wo.delete({
+      where:{
+        id_wo: Number(id)
+      }
+    })
+    res.json(wo)
+  } catch (error) {
+    next(error)
+  }
+});
+
+// ----------- Delivery Order -----------
+//get all tb_do
+router.get('/do', async (req, res, next) => {
+  try {
+    const tb_dos = await prisma.Tb_do.findMany({
+      include: {
+        mst_parts: true,
+        mst_suppliers: true,
+      },
+    });
+  
+      const mst_parts = await prisma.Mst_part.findMany({
+      include:{tb_dos: true}
+      })
+
+      const mst_suppliers = await prisma.Mst_supplier.findMany({
+      include:{tb_dos: true}
+      })
+
+      res.json({tb_dos, mst_parts, mst_suppliers})
+  } catch (error) {
+      next(error)
+  }
+  });
+
+//get tb_do by id
+router.get('/do/:id', async (req, res, next) => {
+  try {
+    const {id} = req.params
+    const tb_dos = await prisma.Tb_do.findUnique({
+      where:{
+        id_do: Number(id)
+      },
+      include: {
+        mst_parts: true,
+        mst_suppliers: true,
+      }
+    })
+    res.json(tb_dos)
+  } catch (error) {
+    next(error)
+  }
+});
+
+//create wo
+router.post('/do', async (req, res, next) => {
+  try {
+      const dos = await prisma.Tb_do.create({
+      data : req.body
+      })
+  
+      res.json({dos})
+  } catch (error) {
+      next(error)
+  }
+  });
+
+//update wo by id
+router.patch('/do/:id', async (req, res, next) => {
+  try {
+    const {id} = req.params
+    const tb_dos = await prisma.Tb_do.update({
+      data: req.body,
+      where:{
+        id_do: Number(id)
+      },
+      include: {
+        mst_parts: true,
+        mst_suppliers: true,
+      }
+    })
+    res.json(tb_dos)
+  } catch (error) {
+    next(error)
+  }
+}); 
+
+//delete wo by id
+router.delete('/do/:id', async (req, res, next) => {
+  try {
+    const {id} = req.params
+    const tb_dos = await prisma.Tb_do.delete({
+      where:{
+        id_do: Number(id)
+      }
+    })
+    res.json(tb_dos)
+  } catch (error) {
+    next(error)
+  }
+});
 
 module.exports = router;
