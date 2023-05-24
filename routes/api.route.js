45edf3d104826e1e1fb7b9e8c0db1cad54b4aab7
zilviazz <zilvia524@gmail.com>
2023-05-24 22:05:56 +0700

    crud rack

--- routes/api.route.js
+++ routes/api.route.js
@@ -6,12 +6,13 @@ router.get('/', async (req, res, next) => {
   res.send({ message: 'Ok api is working 🚀' });
 });
 
+// Get semua isi tabel supplier
 router.get('/supplier', async (req, res) => {
   const supplier = await prisma.Mst_supplier.findMany()
   res.json(supplier)
 })
 
-// Get By:id
+// Get By:id supplier
 router.get('/supplier/:id', async (req, res, next) => {
   try {
       const {id} = req.params
@@ -26,7 +27,7 @@ router.get('/supplier/:id', async (req, res, next) => {
   }
   });
 
-// Create
+// Create supplier
 router.post('/supplier', async (req, res, next) => {
   try {
     const supplier = await prisma.Mst_supplier.create({
@@ -39,7 +40,7 @@ router.post('/supplier', async (req, res, next) => {
   }
 });
 
-// Update
+// Update supplier
 router.patch('/supplier/:id', async (req, res, next) => {
   try {
       const {id} = req.params
@@ -56,7 +57,7 @@ router.patch('/supplier/:id', async (req, res, next) => {
   });
   
 
-// Delete
+// Delete supplier
 router.delete('/supplier/:id', async (req, res, next) => {
   try {
       const {id} = req.params
@@ -70,5 +71,72 @@ router.delete('/supplier/:id', async (req, res, next) => {
       next(error)
   }
   }); 
+
+  // Get semua rack
+router.get('/rack', async (req, res) => {
+  const rack = await prisma.Mst_rack.findMany()
+  res.json(rack)
+})
+
+// Get By:id rack
+router.get('/rack/:id', async (req, res, next) => {
+  try {
+      const {id} = req.params
+      const rack = await prisma.Mst_rack.findUnique({
+      where:{
+          id_rack: Number(id)
+      }
+      })
+      res.json(rack)
+  } catch (error) {
+      next(error)
+  }
+  });
+
+  // Create rack
+router.post('/rack', async (req, res, next) => {
+  try {
+    const rack = await prisma.Mst_rack.create({
+      data : req.body
+    })
+
+    res.json({rack})
+  } catch (error) {
+    next(error)
+  }
+});
+
+// Update rack
+router.patch('/rack/:id', async (req, res, next) => {
+  try {
+      const {id} = req.params
+      const rack = await prisma.Mst_rack.update({
+      data: req.body,
+      where:{
+          id_rack: Number(id)
+      }
+      })
+      res.json(rack)
+  } catch (error) {
+      next(error)
+  }
+  });
+  
+
+// Delete rack
+router.delete('/rack/:id', async (req, res, next) => {
+  try {
+      const {id} = req.params
+      const rack = await prisma.Mst_rack.delete({
+      where:{
+          id_rack: Number(id)
+      }
+      })
+      res.json(rack)
+  } catch (error) {
+      next(error)
+  }
+  });
+
 
 module.exports = router;
