775f17328c50ed11b567f8469362ba351483e2b4
zilviazz <zilvia524@gmail.com>
2023-05-24 10:23:34 +0700

    update api.route.js

--- routes/api.route.js
+++ routes/api.route.js
@@ -4,4 +4,21 @@ router.get('/', async (req, res, next) => {
   res.send({ message: 'Ok api is working 🚀' });
 });
 
+router.get('/supplier', async (req, res) => {
+  const supplier = await prisma.mst_supplier.findMany()
+  res.json(supplier)
+})
+
+router.post('/supplier', async (req, res, next) => {
+  try {
+      const supplier = await prisma.mst_supplier.create({
+      data : req.body
+      })
+  
+      res.json({supplier})
+  } catch (error) {
+      next(error)
+  }
+  });
+
 module.exports = router;
